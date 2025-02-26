06a5899df5131207aa3051021440f95749222467
Felipe Corrêa <105075783+FelipeDev92@users.noreply.github.com>
2025-02-26 13:48:49 -0300

    Update Curso.java

--- src/main/java/Models/Curso.java
+++ src/main/java/Models/Curso.java
@@ -1,4 +1,13 @@
 package Models;
 
+import java.util.ArrayList;
+
 public class Curso {
+    private String nome;
+    private int creditos;
+    private ArrayList<Disciplina> disciplina = new ArrayList<Disciplina>();
+
+    public adicionarDisciplina(){
+        
+    }
 }
