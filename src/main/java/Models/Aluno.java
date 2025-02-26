cafe3aeb247d3ed573bb12714dc7c3e2169c8090
Felipe Corrêa <105075783+FelipeDev92@users.noreply.github.com>
2025-02-26 13:48:20 -0300

    Update Aluno.java

--- src/main/java/Models/Aluno.java
+++ src/main/java/Models/Aluno.java
@@ -1,5 +1,22 @@
 package Models;
 
+import java.util.ArrayList;
+
 public class Aluno {
+    private String nome;
+    private ArrayList<Matricula> matricula = new ArrayList<Matricula>();
+
+    Aluno(String nome, Matricula matricula){
+        this.nome = nome;
+    //    this.matricula = matricula;
+    }
+
+    public void adicionarMatricula(){
+
+    }
+
+    public void removerMatricula(){
+
+    }
 
 }
