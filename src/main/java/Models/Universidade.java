36ccaa4d3c6c8a8bbe445e5c5ca89a1e3f0308c6
Felipe Corrêa <105075783+FelipeDev92@users.noreply.github.com>
2025-03-11 10:33:49 -0300

    Modificação da Classe Universidade.java

--- src/main/java/Models/Universidade.java
+++ src/main/java/Models/Universidade.java
@@ -1,51 +1,15 @@
 package Models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
 public class Universidade {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
-
-    @Column(name =  "nome")
-    @JsonProperty("nome")
     private String nome;
-
-    @ManyToMany
-    @JoinTable(
-            name = "universidade_aluno",
-            joinColumns = @JoinColumn(name = "universidade_id"),
-            inverseJoinColumns = @JoinColumn(name = "aluno_id")
-    )
     private List<Aluno> alunos;
-
-    @ManyToMany
-    @JoinTable(
-            name = "universidade_curso",
-            joinColumns = @JoinColumn(name = "universidade_id"),
-            inverseJoinColumns = @JoinColumn(name = "curso_id")
-    )
     private List<Curso> cursos;
-
-    @ManyToMany
-    @JoinTable(
-            name = "universidade_disciplina",
-            joinColumns = @JoinColumn(name = "universidade_id"),
-            inverseJoinColumns = @JoinColumn(name = "disciplina_id")
-    )
     private List<Disciplina> disciplinas;
-
-    @ManyToMany
-    @JoinTable(
-            name = "universidade_professor",
-            joinColumns = @JoinColumn(name = "universidade_id"),
-            inverseJoinColumns = @JoinColumn(name = "professor_id")
-    )
     private List<Professor> professores;
 
     public Universidade() {}
