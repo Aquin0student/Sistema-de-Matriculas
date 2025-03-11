fdca1cb4824c79aefa16f49001bc53c55203b3b3
Felipe Corrêa <105075783+FelipeDev92@users.noreply.github.com>
2025-03-11 10:32:05 -0300

    Modificação da Classe Curso.java

--- src/main/java/Models/Curso.java
+++ src/main/java/Models/Curso.java
@@ -1,33 +1,13 @@
 package Models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.annotation.Nonnull;
-import jakarta.persistence.*;
-
 import java.util.ArrayList;
 
-@Entity
+
 public class Curso {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "nome")
-    @JsonProperty("nome")
     private String nome;
-
-    @Column(name = "numCreditos")
-    @JsonProperty("creditos")
-    @Nonnull
     private Integer creditos;
-
-    @ManyToMany(mappedBy = "disciplina")
-    @JoinTable(
-            name = "curso_disciplina",
-            joinColumns = @JoinColumn(name = "disciplina_id"),
-            inverseJoinColumns = @JoinColumn(name = "curso_id")
-    )
     private ArrayList<Disciplina> disciplinas = new ArrayList<Disciplina>();
 
     public Curso(String nome, int creditos) {
@@ -40,6 +20,7 @@ public class Curso {
 
     }
 
+    // esses metodos devem estar no controller
     public void adicionarDisciplina(Disciplina disciplina){
         this.disciplinas.add(disciplina);
     }
