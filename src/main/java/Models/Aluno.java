a7578f7a79e9147b935f77e015b6435a933b7af1
Felipe Corrêa <105075783+FelipeDev92@users.noreply.github.com>
2025-03-11 10:31:27 -0300

    Modificação da Classe Aluno.java

--- src/main/java/Models/Aluno.java
+++ src/main/java/Models/Aluno.java
@@ -1,48 +1,32 @@
 package Models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.annotation.Nonnull;
-import jakarta.persistence.*;
-
+import java.io.Serializable;
 import java.util.ArrayList;
 
-@Entity
-@Table(name = "aluno")
 public class Aluno {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "nome")
-    @JsonProperty("nome")
-    @Nonnull
     private String nome;
-
-    @Column(name = "matricula_id")
-    @JsonProperty("matricula_id")
-    @Nonnull
     private Integer matricula;
 
 
 
-    public Aluno(@Nonnull String nome, Matricula matricula){
+    public Aluno( String nome, Matricula matricula){
         this.nome = nome;
         this.matricula = matricula.getNumeroMatricula();
     }
 
-    public Aluno(@Nonnull String nome){
+    public Aluno( String nome){
         this.nome = nome;
     }
 
     public Aluno() {
 
     }
-
+// Esse metodo deve ficar no controller
     public void adicionarMatricula(Matricula matricula){
         this.matricula = matricula.getNumeroMatricula();
     }
-
 
     public void setId(Long id) {
         this.id = id;
